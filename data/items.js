41b45d87b0a4ea88605be2062bd1649d224f3374
naenano-youtube <yuunano370@gmail.com>
2026-02-11 17:13:38 +0900

    サイトファイル

---
+++ data/items.js
@@ -0,0 +1,8 @@
+const distributionItems = [
+    {
+        "id": "1",
+        "title": "Wind from the Far East Remix",
+        "about": "2026/2/11配布。DJHIKAKINさんの楽曲「Wind from the Far East Remix」の創作譜面",
+        "folder": "haihubutsu/DJHK"
+    }
+];
